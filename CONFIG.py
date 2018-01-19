f9909ee284ac361e5726914699d805f724d6491d
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-01-19 09:43:25 +0000

    Add files via upload

---
+++ CONFIG.py
@@ -0,0 +1,22 @@
+'''
+    SHORT URLS BOT CONFIGURATION FILE
+    Edit me before start Bot.py!
+'''
+
+# Token of the bot. Get it one from https://telegram.me/BotFather
+TOKEN = "544616157:AAGfkCQt0vRDFfphf1LyBD81D0UCmSAsqkM"
+# Telegram IDs list of admins of the bot (that can issue /users command)
+ADMINS = [197005208]
+
+# Insert absolute path to database file (.db)
+DATABASE_PATH = "/bfas.db"
+
+# Goo.gl API key. Get it one from https://console.developers.google.com/
+GOOGLE_API_KEY = ""
+
+# AdfLy UserID and API key. Get it one from https://adf.ly/publisher/tools#tools-api
+ADFLY_UID = ""
+ADFLY_API_KEY = ""
+
+# BitLy access token. Get it one from https://bitly.com/a/oauth_apps
+BITLY_ACCESS_TOKEN = ""
